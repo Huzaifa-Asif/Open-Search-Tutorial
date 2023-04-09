85c2f3eb4c0be2064b5745737ca17d011ea7453f
Huzaifa Asif <huzaifa8580@gmail.com>
2023-04-10 04:28:38 +0800

    feat: add express and all the functions are moved into seperate routes

--- app.js
+++ app.js
@@ -1,90 +1,110 @@
+var express = require('express');
+var app = express();
 const { Client } = require('@opensearch-project/opensearch');
+const dotenv = require('dotenv');
+dotenv.config({ path: '.env' });
 
 const client = new Client({
     node: "https://admin:admin@localhost:9200"
 });
-const index_name = 'books';
+const index_name = 'book';
 
-// method to create index
-async function createIndex() {
-    console.log("createIndex")
-    const settings = {
-        settings: {
-          index: {
-            number_of_shards: 4,
-            number_of_replicas: 3,
-          },
-        },
-      };
-
-    const response = await client.indices.create({
-        index: index_name,
-        body: settings,
-    });
-
-    console.log("createIndex response: ", response);
-}
-
-// method to add document in index 
-async function addDocument() {
-    const document = {
-        title: 'The Outsider',
-        author: 'Stephen King',
-        year: '2018',
-        genre: 'Crime fiction',
-    };
-    const id = '5';
-
-    const response = await client.index({
-        id: id,
-        index: index_name,
-        body: document,
-        refresh: true,
-    });
-
-    console.log("add document to index response: ", response.body);
-}
-
-// method to search documents
-async function searchDocuments() {    
-    var query = {
-        query: {
-            match: {
-                title: {
-                    query: 'The Outsider',
+// route to create index
+app.get('/create-index', async (req, res) => {
+    try {
+        const settings = {
+            settings: {
+                index: {
+                    number_of_shards: 1,
+                    number_of_replicas: 1,
                 },
             },
-        },
-    };
+        };
 
-    var response = await client.search({
-        index: index_name,
-        body: query,
-    });
+        const response = await client.indices.create({
+            index: index_name,
+            body: settings,
+        });
 
-    console.log("search documents response: ", response.body.hits);
-}
+        res.send(response);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
 
-// method to delete document
-async function deleteDocument() {
-    var response = await client.delete({
-        index: index_name,
-        id: '1',
-    });
+// route to add document
+app.get('/add-document', async (req, res) => {
+    try {
+        const document = {
+            title: 'The Outsider',
+            author: 'Stephen King',
+            year: '2018',
+            genre: 'Crime fiction',
+        };
+    
+        const response = await client.index({
+            index: index_name,
+            body: document,
+            refresh: true,
+        });
 
-    console.log("delete Document response: ", response.body);
-}
+        res.send(response.body);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
 
-// methid to delete index
-async function deleteIndex() {
-    var response = await client.indices.delete({
-        index: index_name,
-    });
-    console.log("delete Index response: ", response.body);
-}
+// route to search document
+app.get('/search-document', async (req, res) => {
+    try {
+        var query = {
+            query: {
+                match: {
+                    title: {
+                        query: 'The Outsider',
+                    },
+                },
+            },
+        };
+    
+        var response = await client.search({
+            index: index_name,
+            body: query,
+        });    
 
-createIndex();
-addDocument();
-searchDocuments();
-deleteDocument();
-deleteIndex();
+        res.send(response.body.hits);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+// route to search document
+app.get('/delete-document', async (req, res) => {
+    try {
+        var response = await client.delete({
+            index: index_name,
+            id: '1',
+        });
+
+        res.send(response.body);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+// route to delete index
+app.get('/delete-index', async (req, res) => {
+    try {
+        var response = await client.indices.delete({
+            index: index_name,
+        });
+
+        res.send(response.body);
+    } catch (e) {
+        res.status(500).json(e);
+    }
+});
+
+app.listen(3000, () => {
+    console.log('Express app listening on port 3000!');
+});
